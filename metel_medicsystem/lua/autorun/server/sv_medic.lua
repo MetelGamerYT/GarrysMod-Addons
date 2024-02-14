a5b9f558b39c45f3ca7570a245df02b11b495299
Simon Engelbracht <MetelMC.De@yahoo.com>
2024-02-14 20:48:50 +0100

    improved code

--- metel_medicsystem/lua/autorun/server/sv_medic.lua
+++ metel_medicsystem/lua/autorun/server/sv_medic.lua
@@ -1,20 +1,18 @@
-//Made by DerMetelGamerYT Version 1.0
+//Made by DerMetelGamerYT Version 1.3
 
 util.AddNetworkString("metelmedic_deathtimer")
-util.AddNetworkString("metelmedic_RespawnTimer")
-util.AddNetworkString("metelmedic_openplayerinfo")
-util.AddNetworkString("metelmedic_enablebleedingeffect")
-util.AddNetworkString("metelmedic_disablebleedingeffect")
+util.AddNetworkString("metelmedic_showguenther")
+util.AddNetworkString("metelmedic_togglebleeding")
 util.AddNetworkString("metelmedic_isusingbloodpck")
-
-hook.Add("PlayerInitialSpawn", "MetelMedic_DisableZoom", function(ply, key)
-    ply:StopZooming()
-    ply:SetCanZoom(false)
-end)
+util.AddNetworkString("metelmedic_bloodtreat")
+util.AddNetworkString("metelmedic_fractreat")
+util.AddNetworkString("metelmedic_heilth")
+util.AddNetworkString("metelmedic_openselfbody")
 
 //Sorgt das man nicht mehr selbst respawnen kann
 hook.Add("PlayerDeathThink", "MetelMedic_DisableRespawn", function(ply)
-    return false
+    if not ply.respawnTime then return true end
+    return ply.respawnTime < CurTime()
 end)
 
 hook.Add("PostPlayerDeath", "MetelMedic_Death", function(ply)
@@ -22,42 +20,96 @@ hook.Add("PostPlayerDeath", "MetelMedic_Death", function(ply)
 
     for k,v in pairs(ply:GetWeapons()) do
         table.insert(ply.medicdeathweapons, v:GetClass())
-        print(v:GetClass())
     end
-    net.Start("metelmedic_deathtimer")
-    net.Send(ply)
-    if not MetelMedic_Config.NoMedic[ply:getJobTable().name] then
-        timer.Create(ply:SteamID().."_deathtimer", MetelMedic_Config.DieTimer, 1, function()
 
-        end)
-        timer.Create(ply:SteamID().."_RespawnTimer", MetelMedic_Config.RespawnTimer + MetelMedic_Config.DieTimer, 1, function()
-            ply:Spawn()
-        end)
+    if not MetelMedic_Config.NoMedic[ply:getJobTable().name] and ply:GetNWBool("MetelMedic_DieInstant") == false then
+	if not IsValid(ply) then return end
+		timer.Create(ply:SteamID().."_deathtimer", MetelMedic_Config.DieTimer, 1, function()
+            if ply.lragdoll then
+                ply.lragdoll:SetNWInt("respawnTime", 0)
+            end
+		end)
+		timer.Create(ply:SteamID().."_RespawnTimer", MetelMedic_Config.RespawnTimer + MetelMedic_Config.DieTimer, 1, function()
+			ply:Spawn()
+		end)
+
+        ply.respawnTime = CurTime() + MetelMedic_Config.DieTimer
     else
+        ply:SetNWBool("MetelMedic_DieInstant", true)
         timer.Create(ply:SteamID().."_RespawnTimer", MetelMedic_Config.RespawnTimer, 1, function()
             ply:Spawn()
         end)
+
+        ply.respawnTime = CurTime() + MetelMedic_Config.RespawnTimer
     end
+
+    timer.Simple(0.5, function()
+        ply.lragdoll:SetNWInt("respawnTime", ply.respawnTime)
+    end)
+
+    net.Start("metelmedic_deathtimer")
+    net.Send(ply)
 end)
 
-//Entfernt scheiß Piepen beim Sterben lol
+-- Removes shit beeping when dying lol
 hook.Add("PlayerDeathSound", "MetelMedic_MuteSound", function()
     return true
 end)
 
 hook.Add("PlayerSpawn", "MetelMedic_Spawn", function(ply) 
-    net.Start("metelmedic_disablebleedingeffect")
+    net.Start("metelmedic_togglebleeding")
+        net.WriteBool(false)
     net.Send(ply)
     if timer.Exists(ply:SteamID().."_RespawnTimer") then
         timer.Destroy(ply:SteamID().."_RespawnTimer")
     end
-    if ply:GetNWBool("metelmedic_isonstretcher", false) then
-        local ent = ply:GetNWEntity("metelmedic_isonstretcher_entity", nil)
-        ent:Remove()
-        print(ply:GetNWEntity("metelmedic_isonstretcher_enity", nil))
-        ply:SetNWBool("metelmedic_isonstretcher", false)
-    end
+    ply:StopZooming()
+    ply:SetCanZoom(false)
 
+    -- Setzt Player Infos
+    MetelMedic_FullHeal(ply)
+    MetelMedic_RemoveRag(ply)
+end)
+
+-- Erstellt die Ragdoll
+hook.Add("CreateEntityRagdoll","MeteMedic_CreateRagdoll",function(ply,rag)
+    ply.lragdoll = rag
+    ply.lragdoll:SetNWString("metel_ragdollowner", ply)
+    ply.lragdoll:SetHealth(300)
+    ply.lragdoll:SetMaxHealth(300)
+    ply.lragdoll:SetNWEntity("metel_ragdollowner_ent", ply)    
+    ply.lragdoll:SetNWInt("respawnTime", MetelMedic_Config.DieTimer)
+    ply:Spectate(OBS_MODE_CHASE)
+    ply:SpectateEntity(rag)
+    ply:SetShouldServerRagdoll(true)
+end)
+
+hook.Add("PlayerButtonDown", "MetelMedic_DragRagdoll", function(ply, button)
+    if button == KEY_B then
+        net.Start("metelmedic_openselfbody")
+        net.Send(ply)
+    end
+    -- Ragdoll ziehen
+    tr = {}
+    tr.start = ply:GetShootPos()
+    tr.endpos = ply:GetShootPos() + ( ply:GetAimVector() * 95 )
+    tr.filter = ply
+    tr.mask = MASK_SHOT
+    trace = util.TraceLine(tr)
+    if IsValid(trace.Entity) and trace.Entity:GetPos():Distance(ply:GetPos()) > 512 then return end
+    if not trace.Entity:IsRagdoll() then return end
+
+    if button == KEY_R then
+        if trace.Entity:IsPlayerHolding() then return end
+        ply:PickupObject(trace.Entity)
+    end
+end)
+
+hook.Add("PlayerDisconnected", "MetelMedic_RemoveRagdoll", function(ply)
+    MetelMedic_RemoveRag(ply)
+end)
+
+function MetelMedic_RemoveRag(ply)
     if ply.lragdoll and IsValid(ply.lragdoll) then
         if not ply.ragdoll == nil then
             ply.lragdoll:Remove()
@@ -68,31 +120,24 @@ hook.Add("PlayerSpawn", "MetelMedic_Spawn", function(ply)
     end
     ply:SetShouldServerRagdoll(true)
 
-    //Set Player Infos
+    if ply:GetNWBool("metelmedic_isonstretcher", false) then
+        local ent = ply:GetNWEntity("metelmedic_isonstretcher_entity", nil)
+        ent:Remove()
+        print(ply:GetNWEntity("metelmedic_isonstretcher_enity", nil))
+        ply:SetNWBool("metelmedic_isonstretcher", false)
+    end
+end
+
+function MetelMedic_FullHeal(ply)
     ply:SetNWString("MetelMedic_Bleeding", "none")
     ply:SetNWBool("MetelMedic_Brokenleg", false)
     ply:SetNWInt("MetelMedic_Blood", 100)
-    ply.walkspeed = ply:GetWalkSpeed()
-    ply.duckspeed = ply:GetDuckSpeed()
-    ply.runspeed = ply:GetRunSpeed()
+    ply:SetNWBool("MetelMedic_DieInstant", false)
 
+    net.Start("metelmedic_togglebleeding")
+    net.WriteBool(false)
+    net.Send(ply)
     if timer.Exists("MetelMedic_BleedingTimer_"..ply:SteamID64()) then
         timer.Destroy("MetelMedic_BleedingTimer_"..ply:SteamID64())
     end
-end)
-
-// Erstellt die Ragdoll
-hook.Add("CreateEntityRagdoll","MeteMedic_CreateRagdoll",function(ply,rag)
-    ply.lragdoll = rag
-    ply.lragdoll:SetNWString("metel_ragdollowner", ply)
-    ply:Spectate(OBS_MODE_CHASE)
-    ply:SpectateEntity(rag)
-    ply:SetShouldServerRagdoll(true)
-end)
-
-hook.Add("PlayerButtonDown", "MetelMedic_OpenBodyInfo", function(ply, button)
-    if button == KEY_B then
-        net.Start("metelmedic_openplayerinfo")
-        net.Send(ply)
-    end
-end)
+end
--- metel_medicsystem/lua/autorun/server/sv_medicbpb.lua
+++ metel_medicsystem/lua/autorun/server/sv_medicbpb.lua
@@ -3,26 +3,27 @@ hook.Add("PostEntityTakeDamage", "MetelMedic_TakeDamage", function(ent, dmg)
         local ply = ent
         if MetelMedic_Config.NoMedic[ent:getJobTable().name] then return end
         if dmg:IsBulletDamage() then
+            dmg:ScaleDamage(0.9)
             local percent = math.random(1, 100)
             local chance = 100 - MetelMedic_Config.BleedingPercent
 
             if percent >= chance then
-                if dmg:GetDamage() <= 25 then
+                if dmg:GetDamage() <= 15 then
                     ent:SetNWString("MetelMedic_Bleeding", "light")
                     MetelMedic_StartBleeding(ent, 2, 3)
-                elseif dmg:GetDamage() >= 26 and dmg:GetDamage() <= 74 then
+                elseif dmg:GetDamage() >= 26 and dmg:GetDamage() <= 69 then
                     ent:SetNWString("MetelMedic_Bleeding", "medium")
                     MetelMedic_StartBleeding(ent, 4, 5)
-                elseif dmg:GetDamage() >= 75 then
+                elseif dmg:GetDamage() >= 70 then
                     ent:SetNWString("MetelMedic_Bleeding", "heavy")
                     MetelMedic_StartBleeding(ent, 5, 10)
                 end
-                //print(percent)
-                //print("Blutung")
-                net.Start("metelmedic_enablebleedingeffect")
+                net.Start("metelmedic_togglebleeding")
+                net.WriteBool(true)
                 net.Send(ent)
             end
         elseif dmg:IsFallDamage() then // Fügt Brüche hinzu
+            dmg:ScaleDamage(0.6)
             if dmg:GetDamage() >= 40 then
                 MetelMedic_StartLegFracture(ent)
             end
@@ -31,25 +32,79 @@ hook.Add("PostEntityTakeDamage", "MetelMedic_TakeDamage", function(ent, dmg)
 end)
 
 function MetelMedic_StartBleeding(plystring, damage, bloodlose)  
-    if timer.Exists("MetelMedic_BleedingTimer_"..plystring:SteamID64()) and plystring:GetNWInt("MetelMedic_Blood") <= 0 then
-        timer.Destroy("MetelMedic_BleedingTimer_"..plystring:SteamID64())
-        plystring:Kill()
-        return
-    end
+    if not MetelMedic_Config.NoMedic[plystring:getJobTable().name] then
+        if timer.Exists("MetelMedic_BleedingTimer_"..plystring:SteamID64()) and plystring:GetNWInt("MetelMedic_Blood") <= 0 then
+            timer.Destroy("MetelMedic_BleedingTimer_"..plystring:SteamID64())
+            plystring:Kill()
+            return
+        end
 
-    for k,v in pairs(plystring:GetNWString("MetelMedic_Bleeding")) do
-        timer.Create("MetelMedic_BleedingTimer_"..plystring:SteamID64(), 5, 0, function()
-            plystring:TakeDamage(damage,ent,ent)
-            plystring:SetNWInt("MetelMedic_Blood", plystring:GetNWInt("MetelMedic_Blood", nil) - bloodlose)
-        end)
+        --for k,v in pairs(plystring:GetNWString("MetelMedic_Bleeding")) do
+            timer.Create("MetelMedic_BleedingTimer_"..plystring:SteamID64(), 7, 0, function()
+				local bloodl = bloodlose
+                plystring:TakeDamage(damage,ent,ent)
+                plystring:SetNWInt("MetelMedic_Blood", plystring:GetNWInt("MetelMedic_Blood", nil) - bloodl)
+                local percent = math.random(1, 100)
+                local chance = 100 - MetelMedic_Config.BleedingStopPercent
+
+                if percent >= chance then
+                    timer.Destroy("MetelMedic_BleedingTimer_"..plystring:SteamID64())
+                    plystring:SetNWString("MetelMedic_Bleeding", "none")
+					net.Start("metelmedic_togglebleeding")
+                    net.WriteBool(false)
+					net.Send(plystring)
+                end
+            end)
+        --end
     end
 end
 
 function MetelMedic_StartLegFracture(plystrings)
-    plystrings:SetNWBool("MetelMedic_Brokenleg", true)
-    if plystrings:GetNWBool("MetelMedic_Brokenleg") == true then
-        plystrings:SetDuckSpeed(100)
-        plystrings:SetWalkSpeed(100)
-        plystrings:SetRunSpeed(100)
+    if not MetelMedic_Config.NoMedic[plystrings:getJobTable().name] then
+        plystrings:SetNWBool("MetelMedic_Brokenleg", true)
+        if plystrings:GetNWBool("MetelMedic_Brokenleg") == true then
+            plystrings:SetDuckSpeed(100)
+            plystrings:SetWalkSpeed(100)
+            plystrings:SetRunSpeed(100)
+        end
     end
 end
+
+-- Behandlungen von Günther
+net.Receive("metelmedic_bloodtreat", function(len, ply)
+    if ply:GetNWInt("MetelMedic_Blood") >= 100 then
+        ply:ChatPrint("[Günther] You already have enough blood in your body, why do you need more?")
+    else
+        ply:SetNWInt("MetelMedic_Blood", 100)
+		timer.Destroy("MetelMedic_BleedingTimer_"..ply:SteamID64())
+		ply:SetNWString("MetelMedic_Bleeding", "none")
+		net.Start("metelmedic_togglebleeding")
+        net.WriteBool(false)
+		net.Send(ply)
+        ply:ChatPrint("[Günther] I have now injected you with new blood.")
+        ply:setDarkRPVar("money", ply:getDarkRPVar("money") - 5)
+    end
+end)
+
+net.Receive("metelmedic_fractreat", function(len, ply)
+    if not ply:GetNWBool("MetelMedic_Brokenleg") then
+        ply:ChatPrint("[Günther] You don't have a hernia, or can't I see it?")
+    else
+        ply:SetNWBool("MetelMedic_Brokenleg", false)
+        ply:SetDuckSpeed(ply.duckspeed)
+        ply:SetWalkSpeed(ply.walkspeed)
+        ply:SetRunSpeed(ply.runspeed)
+        ply:ChatPrint("[Günther] I have now dealt with your faction(s).")
+        ply:setDarkRPVar("money", ply:getDarkRPVar("money") - 10)
+    end
+end)
+
+net.Receive("metelmedic_heilth", function(len,ply)
+    if ply:Health() == ply:GetMaxHealth() then
+        ply:ChatPrint("[Günther] Are you not healed yet?")
+    else
+        ply:SetHealth(ply:GetMaxHealth())
+        ply:ChatPrint("[Günther] You have now been successfully treated by me.")
+        ply:setDarkRPVar("money", ply:getDarkRPVar("money") - 75)
+    end
+end)
