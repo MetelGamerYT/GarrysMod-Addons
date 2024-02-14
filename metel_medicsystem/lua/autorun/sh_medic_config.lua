2b6376085e638156f7df95bc227873d099358387
Simon Engelbracht <MetelMC.De@yahoo.com>
2024-02-14 20:49:09 +0100

    improved Config

--- metel_medicsystem/lua/autorun/sh_medic_config.lua
+++ metel_medicsystem/lua/autorun/sh_medic_config.lua
@@ -1,13 +1,27 @@
-//Made by DerMetelGamerYT Version 1.0
+--Made by DerMetelGamerYT Version 1.2.1
 
 MetelMedic_Config = {}
 
-MetelMedic_Config.DieTimer = 15 // How long until you die
+MetelMedic_Config.DieTimer = 90 -- How long until you die / Lie dying
 
-MetelMedic_Config.RespawnTimer = 5 // How long it takes to respawn after death
+MetelMedic_Config.RespawnTimer = 10 -- How long it takes to respawn after death
 
-MetelMedic_Config.BleedingPercent = 25 // What is the chance of Bullets to bleed
+MetelMedic_Config.BleedingPercent = 5 -- How high is the chance of bleeding from bullets
 
-MetelMedic_Config.NoMedic = { // Which jobs should be ignored by the Medic system (bleeds, breaks, respawns)?
+MetelMedic_Config.BleedingStopPercent = 10 -- What is the chance that bleeding will stop on its own?
+
+-- Which jobs cannot bleed and respawn in RespawnTimer seconds
+MetelMedic_Config.NoMedic = {
+    ["Hobo"] = true,
+    ["Staff"] = true,
+}
+
+-- Which jobs are medical jobs
+MetelMedic_Config.Medics = {
+    ["Medic"] = true,
+}
+
+-- Which jobs do not bleed (effect, does not always work at the moment)
+MetelMedic_Config.NoBloodEffects = {
     ["Hobo"] = true,
 }
