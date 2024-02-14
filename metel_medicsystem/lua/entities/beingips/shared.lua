c8420427a537d91a3260caeb97988fd361ece3cd
Simon Engelbracht <MetelMC.De@yahoo.com>
2024-02-14 20:49:32 +0100

    Update shared.lua

--- metel_medicsystem/lua/entities/beingips/shared.lua
+++ metel_medicsystem/lua/entities/beingips/shared.lua
@@ -1,7 +1,7 @@
 ENT.Type = "anim"
 ENT.Base = "base_gmodentity"
 
-ENT.PrintName = "Cast"
-ENT.Category = "MetelMedic"
+ENT.PrintName = "Plaster"
+ENT.Category = "MetelGaming Medic"
 ENT.Spawnable = true
 ENT.AdminSpawnable = true
